0ec39fae48eb4dda17606cbe7242367f7a125866
mat-czap <czapranski.mateusz@gmail.com>
2021-05-08 21:50:40 +0200

    add tests

--- src/events/services/impl/events-mock.service.spec.ts
+++ src/events/services/impl/events-mock.service.spec.ts
@@ -57,6 +57,83 @@ describe('EventsMockService', () => {
       expect(eventsService.getEvents).toBeDefined();
       expect(typeof eventsService.getEvents).toBe('function');
     });
+
+    it('should return first 3 events', async () => {
+      const numberEvents = 3;
+      const sample = [
+        {
+          id: '25ac2e05-b1e8-47b4-b46c-c0bd7004bfa9',
+          title: 'Exceptional Group',
+          startDate: '2020-01-01T09:00:00.000Z',
+          endDate: '2020-01-01T11:00:00.000Z',
+        },
+        {
+          id: 'd32aa30d-337b-49c8-bb98-d5f3e2388170',
+          title: 'Shindig Red Inc',
+          startDate: '2020-01-01T12:00:00.000Z',
+          endDate: '2020-01-01T14:00:00.000Z',
+        },
+        {
+          id: 'fd198a9e-f24a-4f92-9716-ecefeac71bd1',
+          title: 'Vox Scene Bliss',
+          startDate: '2020-01-01T15:00:00.000Z',
+          endDate: '2020-01-01T17:00:00.000Z',
+        },
+      ];
+
+      const startDate = '2020-01-01T09:00:00.000Z';
+      const endData = '2020-01-01T15:00:00.000Z';
+
+      const result = await eventsService.getEvents(startDate, endData, 0, 0);
+      expect(result.totalCount).toBe(numberEvents);
+      expect(result.events).toEqual(sample);
+    });
+
+    it('should return events numbers 2 and 3 (instead of 1-3) because of offset 1', async () => {
+      const expectedEventsNum = 2;
+      const expectedEvents = [
+        {
+          id: 'd32aa30d-337b-49c8-bb98-d5f3e2388170',
+          title: 'Shindig Red Inc',
+          startDate: '2020-01-01T12:00:00.000Z',
+          endDate: '2020-01-01T14:00:00.000Z',
+        },
+        {
+          id: 'fd198a9e-f24a-4f92-9716-ecefeac71bd1',
+          title: 'Vox Scene Bliss',
+          startDate: '2020-01-01T15:00:00.000Z',
+          endDate: '2020-01-01T17:00:00.000Z',
+        },
+      ];
+
+      const exprectedResult = { totalCount: expectedEventsNum, events: expectedEvents };
+
+      const startDate = '2020-01-01T09:00:00.000Z';
+      const endData = '2020-01-01T15:00:00.000Z';
+
+      const result = await eventsService.getEvents(startDate, endData, 1, 0);
+      expect(result).toMatchObject(exprectedResult);
+    });
+
+    it('should return events numbers 2 (instead of 1-3) because of offset 1 and limit 1 ', async () => {
+      const expectedEventsNum = 1;
+      const expectedEvents = [
+        {
+          id: 'd32aa30d-337b-49c8-bb98-d5f3e2388170',
+          title: 'Shindig Red Inc',
+          startDate: '2020-01-01T12:00:00.000Z',
+          endDate: '2020-01-01T14:00:00.000Z',
+        },
+      ];
+
+      const exprectedResult = { totalCount: expectedEventsNum, events: expectedEvents };
+
+      const startDate = '2020-01-01T09:00:00.000Z';
+      const endData = '2020-01-01T15:00:00.000Z';
+
+      const result = await eventsService.getEvents(startDate, endData, 1, 1);
+      expect(result).toMatchObject(exprectedResult);
+    });
   });
 
   describe('removeEvent()', () => {
