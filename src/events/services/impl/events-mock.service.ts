5226940876531c56990a356fc7dc4883aad43a39
mat-czap <czapranski.mateusz@gmail.com>
2021-05-08 21:53:01 +0200

    add getEvents implementation

--- src/events/services/impl/events-mock.service.ts
+++ src/events/services/impl/events-mock.service.ts
@@ -2,6 +2,8 @@ import { EventNotFound } from './../events.service';
 import { EventsService } from '../events.service';
 import { Event } from '../../models/event';
 
+type getEventsResult = { totalCount: number; events: Event[] };
+
 /* eslint-disable */
 export class EventsMockService implements EventsService {
   constructor(private _events: Event[]) {}
@@ -25,8 +27,25 @@ export class EventsMockService implements EventsService {
     offset: number,
     limit: number,
   ): Promise<{ totalCount: number; events: Event[] }> {
-    // @ts-ignore
-    return Promise.resolve({}); // todo: implement method
+    const [start, end] = [dateFrom, dateTo].map((date) => new Date(date));
+
+    const isBetweenDates = (event: Event) => new Date(event.startDate) >= start && new Date(event.startDate) <= end;
+
+    const makeResult = (total: getEventsResult, event: Event) => {
+      total.totalCount++;
+      total.events.push(event);
+      return total;
+    };
+
+    const initialVal: getEventsResult = { totalCount: 0, events: [] };
+
+    const result: getEventsResult = this._events
+      .filter((event) => isBetweenDates(event))
+      .slice(offset || 0)
+      .slice(0, limit || undefined)
+      .reduce(makeResult, initialVal);
+
+    return Promise.resolve(result);
   }
 
   removeEvent(id: string): Promise<void> {
